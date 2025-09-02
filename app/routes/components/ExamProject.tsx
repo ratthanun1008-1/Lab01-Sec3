4fcf6557f7cce609cffe5b54a0888f47f63719f3
ratthanun1008-1 <ratthanun.ruk@rmutto.ac.th>
2025-09-02 18:16:45 +0700

    components

---
+++ app/routes/components/ExamProject.tsx
@@ -0,0 +1,14 @@
+export const ExamProject =[
+    {
+    Code: String,
+    Title: String,
+    Logo: String, // จัดเก็บ Image
+    Description: String,
+    Category: Integer,
+    Owner: String,
+    LastestVersion: String,
+    Price: Double,
+    Website: String, // เว็บไซต์หลักของเครื่องมือที่นาเสนอที่สามารถเข้าถึงได้
+    Top10: Boolean
+    }
+];
