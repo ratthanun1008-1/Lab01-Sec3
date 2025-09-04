320ddc2b64ab92381cf385bb3a45df928b051562
ratthanun1008-1 <ratthanun.ruk@rmutto.ac.th>
2025-09-04 10:21:19 +0700

    components

--- app/routes/components/ExamProject.tsx
+++ app/routes/components/ExamProject.tsx
@@ -1,14 +1,20 @@
-export const ExamProject =[
+export const myExamProject =[
     {
-    Code: String,
-    Title: String,
-    Logo: String, // จัดเก็บ Image
-    Description: String,
-    Category: Integer,
-    Owner: String,
-    LastestVersion: String,
-    Price: Double,
-    Website: String, // เว็บไซต์หลักของเครื่องมือที่นาเสนอที่สามารถเข้าถึงได้
-    Top10: Boolean
+    tpId: 1,
+    tpTitle: "Colleen",
+    tpDescription: "Colleen Shop",
+    tpLogo: "/images/colorPencil.png",
+    tpPrice: 199,
+    tpWebsite: "https://www.colleenpencil.co.th/",
+    tpTop10: true
+    },
+    {
+    tpId: 2,
+    tpTitle: "Pen and Pencil Shop",
+    tpDescription: "Studying Shop",
+    tpLogo: "/images/pen.png",
+    tpPrice: 200,
+    tpWebsite: "https://www.b2s.co.th/en/" ,
+    tpTop10: false
     }
 ];
